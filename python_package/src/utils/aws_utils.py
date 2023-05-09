1cdb48c1a343e5468ff0a8c13006aab3bf2860d7
Florent-COMPAGNONI <florent.compagnoni+dev@gmail.com>
2023-05-09 19:10:54 +0200

    textract refacto

---
+++ python_package/src/utils/aws_utils.py
@@ -0,0 +1,20 @@
+import boto3
+
+from mypy_boto3_textract.client import TextractClient
+from mypy_boto3_s3.service_resource import S3ServiceResource, Object
+
+
+DEFAULT_REGION = "eu-west-1"
+
+
+def get_s3_connection() -> S3ServiceResource:
+    return boto3.resource("s3")
+
+
+def get_textract_client(region=DEFAULT_REGION) -> TextractClient:
+    return boto3.client("textract", region_name=region)
+
+
+def write_in_s3(binary_file: bytes, s3_connection: S3ServiceResource, bucket, object):
+    s3_file: Object = s3_connection.Object(bucket, object)
+    s3_file.put(Body=binary_file)
