c03a85f75fb2c7d85f03b8a5b89861e81cf7403d
Marvin CHERRIERE <mcherriere.pro@gmail.com>
2023-06-19 10:16:30 +0200

    adding MPS options

--- python_package/src/comics_generation/comic_diffusion.py
+++ python_package/src/comics_generation/comic_diffusion.py
@@ -1,3 +1,4 @@
+import torch
 from diffusers import DiffusionPipeline
 from PIL.Image import Image
 
@@ -7,7 +8,7 @@ ARTSTYLE = ", pepelarraz artstyle"
 
 
 def generate_images(pipeline: DiffusionPipeline, prompt: str) -> list[Image]:
-    kwargs = format_prompt(prompt, num_inference_steps=1, guidance_scale=1)
+    kwargs = format_prompt(prompt)
     output = pipeline(**kwargs)
 
     return output.images
@@ -16,20 +17,20 @@ def generate_images(pipeline: DiffusionPipeline, prompt: str) -> list[Image]:
 def format_prompt(
     prompt: str,
     num_images_per_prompt: int = 1,
-    num_inference_steps: int = 50,
-    guidance_scale: float = 7.5,
+    num_inference_steps: int = 60,
+    guidance_scale: float = 10,
 ) -> dict:
     return {
         "prompt": prompt + ARTSTYLE,
         # "height": ,
         # "width": ,
         "num_inference_steps": num_inference_steps,  # More denoising steps usually lead to a higher quality image
-        # "negative_prompt": ,
+        "negative_prompt": "ugly, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, extra limbs, disfigured, deformed, body out of frame, bad anatomy, watermark, signature, cut off, low contrast, underexposed, overexposed, bad art, beginner, amateur, distorted face, blurry, draft, grainy",
         "num_images_per_prompt": num_images_per_prompt,
-        # "generator": ,   # torch.generator("cuda") tu use for GPU
         # "output_type": , # can be PIL.Image or np.array
         # "return_dict": True,  # boolean, to test
         "guidance_scale": guidance_scale,  # Higher guidance scale encourages to generate images that are closely linked to the text prompt
+        "generator": torch.Generator("mps"),  # torch.generator("cuda") tu use for GPU
     }
 
 
@@ -47,7 +48,12 @@ def init_pipeline(pretrained_model_name_or_path: str = COMIC_DIFFUSION_REPOSITOR
     )
 
 
-pipeline = init_pipeline(COMIC_DIFFUSION_REPOSITORY)
-prompt = "batman on a roof, nightsky, city in background"
+#  testing if OS is macOs
+if torch.backends.mps.is_available():
+    pipeline = init_pipeline(COMIC_DIFFUSION_REPOSITORY).to("mps")
+else:
+    pipeline = init_pipeline(COMIC_DIFFUSION_REPOSITORY)
+
+prompt = "spider-man on a roof, sunlight, city in background"
 images = generate_images(pipeline, prompt)
 save_images(images)
