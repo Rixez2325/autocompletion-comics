8753ce81df71e582f7aedb082fa3ad728350f0e5
Florent-COMPAGNONI <florent.compagnoni+dev@gmail.com>
2023-07-28 11:00:21 +0200

    update typing

--- python_package/src/images_preparation/panel_segmentation.py
+++ python_package/src/images_preparation/panel_segmentation.py
@@ -66,11 +66,11 @@ def is_bboxes_overlaping(a: Tuple, b: Tuple) -> bool:
     return a[0] < b[2] and a[2] > b[0] and a[1] < b[3] and a[3] > b[1]
 
 
-def merge_bboxes(a: Tuple, b: Tuple) -> tuple:
+def merge_bboxes(a: Tuple, b: Tuple) -> Tuple:
     return (min(a[0], b[0]), min(a[1], b[1]), max(a[2], b[2]), max(a[3], b[3]))
 
 
-def refine_regions_into_panels(regions: List, img_shape: Tuple) -> List[tuple]:
+def refine_regions_into_panels(regions: List, img_shape: Tuple) -> List[Tuple]:
     panels_bbox = []
     for region in regions:
         for i, panel in enumerate(panels_bbox):
@@ -83,7 +83,7 @@ def refine_regions_into_panels(regions: List, img_shape: Tuple) -> List[tuple]:
     return remove_small_panels(panels_bbox, img_shape)
 
 
-def remove_small_panels(panels_bbox: List[tuple], img_shape: Tuple) -> List[tuple]:
+def remove_small_panels(panels_bbox: List[Tuple], img_shape: Tuple) -> List[Tuple]:
     for i, panel in reversed(list(enumerate(panels_bbox))):
         img_area = (panel[2] - panel[0]) * (panel[3] - panel[1])
         if img_area < 0.01 * img_shape[0] * img_shape[1]:
@@ -92,7 +92,7 @@ def remove_small_panels(panels_bbox: List[tuple], img_shape: Tuple) -> List[tupl
 
 
 def cut_panels_from_source(
-    src_img: np.ndarray, panels_bbox: List[tuple]
+    src_img: np.ndarray, panels_bbox: List[Tuple]
 ) -> List[np.ndarray]:
     panels = []
     for bbox in panels_bbox:
